6199717b60d68017202be6030d73d7def38aeb55
Azimeh <azimehobadiah1@gmail.com>
2026-04-05 00:04:20 +0100

    feat: add My Catalogue page and components, add icon for removing catalogue item to ResourceCard component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import MyCatalogues from './pages/MyCatalogues';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/catalogues" element={<MyCatalogues />} />
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Routes>
     </BrowserRouter>
